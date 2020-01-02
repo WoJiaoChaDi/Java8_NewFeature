5a84e8c61ce118ae440878b9ea573144447b26ed
wojiaochadi <264835229@qq.com>
2020-01-02 15:57:21 +0800

    并行流、串行流
    Fork/Join 框架：
    当任务的复杂度比较高的时候，超过拆分所消耗的运算的临界值，那么ForkJoin框架能更进一步使用cpu资源，提高效率

--- src/main/java/com/atguigu/java8/forkJoin/TestForkJoin.java
+++ src/main/java/com/atguigu/java8/forkJoin/TestForkJoin.java
@@ -6,6 +6,7 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.ForkJoinTask;
+import java.util.stream.LongStream;
 
 public class TestForkJoin {
 
@@ -38,5 +39,23 @@ public class TestForkJoin {
 
         Instant end = Instant.now();
         System.out.println("消费时间为：" + Duration.between(start, end).toMillis());
+    }
+
+    @Test
+    public void test3() {
+        Instant start = Instant.now();
+        //顺序流（单线程）
+        LongStream.rangeClosed(0, 20000000000L)
+                .reduce(0, Long::sum);
+        Instant end = Instant.now();
+        System.out.println("消费时间为：" + Duration.between(start, end).toMillis());
+
+        Instant start1 = Instant.now();
+        //并行流\顺序流转换 .parallel() .sequential()
+        LongStream.rangeClosed(0, 20000000000L)
+                .parallel()
+                .reduce(0, Long::sum);
+        Instant end1 = Instant.now();
+        System.out.println("消费时间为：" + Duration.between(start1, end1).toMillis());
     }
 }
